cade9d72f5614485861d704d2815902581fa2f97
Jalu <ck97756@yahoo.com.tw>
2016-02-18 05:24:43 +0800

    Add header

--- index.js
+++ index.js
@@ -1,7 +1,69 @@
-var Main=React.createClass({
-	render:function(){
-		return(
-			
+var Navigator = React.createClass({
+	displayName: "Navigator",
+
+	render: function render() {
+		return React.createElement(
+			"ul",
+			null,
+			React.createElement(
+				"li",
+				null,
+				React.createElement(
+					"a",
+					{ href: "./index.html" },
+					"CG2016"
+				)
+			),
+			React.createElement(
+				"li",
+				null,
+				React.createElement(
+					"a",
+					{ href: "./about.html" },
+					"About"
+				)
+			),
+			React.createElement(
+				"li",
+				null,
+				React.createElement(
+					"a",
+					{ href: "./course.html" },
+					"Course"
+				)
+			),
+			React.createElement(
+				"li",
+				null,
+				React.createElement(
+					"a",
+					{ href: "./Homework.html" },
+					"Homework"
+				)
+			),
+			React.createElement(
+				"li",
+				null,
+				React.createElement(
+					"a",
+					{ href: "./Project.html" },
+					"Project"
+				)
+			)
 		);
 	}
 });
+
+var Main = React.createClass({
+	displayName: "Main",
+
+	render: function render() {
+		return React.createElement(
+			"header",
+			null,
+			React.createElement(Navigator, null)
+		);
+	}
+});
+
+ReactDOM.render(React.createElement(Main, null), document.getElementById("main"));
